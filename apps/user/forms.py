7d4c2da17c2ef1eec6896a949970ed6c73599afd
madlife <madlifedilan@gmail.com>
2022-04-05 17:13:56 +0800

    admin

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -10,6 +10,7 @@ class RegisterForm(forms.Form):
     attribute = (
         ('教师', 'teacher'),
         ('学生', 'student'),
+        ('管理员', 'admin')
     )
     username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
