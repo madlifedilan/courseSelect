9a2f427f601b19c6acab3e2e59c4ffbc8f7ff656
madlife <madlifedilan@gmail.com>
2022-04-10 16:21:57 +0800

    黑名单中间件rename

--- utils/Middleware.py
+++ utils/Middleware.py
@@ -6,7 +6,8 @@ from django.core.cache import cache
 
 # 频率限制访问
 class IpLimitMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+    @staticmethod
+    def process_request(request):
         ip = request.META.get("REMOTE_ADDR")
 
         # 获取黑名单
@@ -18,20 +19,20 @@ class IpLimitMiddleware(MiddlewareMixin):
         requests = cache.get(ip, [])
 
         # 如果值存在，且当前时间 - 最后一个时间 > 30 则清洗掉这个值  这里我们插入请求的时间为头插
-        while requests and time.time() - requests[-1] > 60:
+        while requests and time.time() - requests[-1] > 30:
             requests.pop()
 
         # 若没有存在值，则添加，过期时间为30秒，这个过期时间与上面判断的30 保持一致
         requests.insert(0, time.time())
-        cache.set(ip, requests, timeout=60)
+        cache.set(ip, requests, timeout=30)
 
-        # 如果访问次数大于30次，假如黑名单，封2分钟
+        # 如果访问次数大于50次，加入黑名单，封2分钟
         if len(requests) > 30:
             black_list.append(ip)
             cache.set('black', black_list, timeout=60 * 2)
 
-        # 限制访问次数为 5 次
+        # 限制访问次数为 10 次
         print(len(requests))
-        if len(requests) > 5:
+        if len(requests) > 10:
             return HttpResponse("请求过于频繁，请稍后重试！")
 
